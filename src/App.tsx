d3eef5df0ea6e59816bff4c011e27ede2f38a6d6
blck-snwmn <whitesnowmancreator@gmail.com>
2022-05-22 11:45:35 +0900

    Chage form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { useForm } from "react-hook-form";
 
+
+
 function App() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = handleSubmit(data => console.log(data));
 
-  console.log(watch("example"));
+  console.log(watch("point"));
 
   const [count, setCount] = useState(0)
 
@@ -20,23 +22,26 @@ function App() {
         </p>
       </header>
       <form onSubmit={onSubmit}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        <input defaultValue="test" {...register("type")} />
+        <div>
+          <input {...register("requiredInput", { required: true })} />
+          {errors.requiredInput && <span style={{ color: 'red' }}>This field is required</span>}
+        </div>
         <div>
           <input {...register("add[0]")} />
           <input {...register("add[1]")} />
           <input {...register("add[2]")} />
           <input {...register("add[3]")} />
         </div>
-
+        <div>
+          <select {...register("point")}>
+            <option value="0">100</option>
+            <option value="1">500</option>
+          </select>
+        </div>
         <input type="submit" />
-      </form>
-    </div>
+      </form >
+    </div >
   )
 }
 
